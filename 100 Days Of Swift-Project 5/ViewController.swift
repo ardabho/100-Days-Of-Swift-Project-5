8ac2174021b37594b03a0d8e9f33448258c67341
Arda Büyükhatipoğlu <ardabho@gmail.com>
2022-06-15 21:41:19 +0300

    Day 28 finished

--- 100 Days Of Swift-Project 5/ViewController.swift
+++ 100 Days Of Swift-Project 5/ViewController.swift
@@ -27,7 +27,7 @@ class ViewController: UITableViewController {
             allWords = ["silkworm"]
         }
         
-        //...
+        startGame()
     }
     
     @objc func promptForAnswer() {
@@ -43,7 +43,72 @@ class ViewController: UITableViewController {
         present(ac, animated: true)
     }
     
+    //MARK: - Answer Validation functions:
+    
+    /// Check if the word user typed is possible with the given words letters.
+    /// - Parameter word: Word ( String ) entered by the user
+    /// - Returns: Bool
+    func isPossible(word: String) -> Bool {
+        guard var tempword = title?.lowercased() else { return false}
+        
+        for letter in word {
+            if let position = tempword.firstIndex(of: letter) {
+                tempword.remove(at: position)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+    
+    ///Checks if the word is entered before by the user
+    func isOriginal(word: String) -> Bool {
+        return !usedWords.contains(word)
+    }
+    
+    ///Checks if the word is a real word by comparing it with allWords
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let mispelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return mispelledRange.location == NSNotFound
+    }
+    
+    // Submit the word
     func submit(_ answer: String) {
+        
+        let lowerCased = answer.lowercased()
+        let errorTitle: String
+        let errorMessage: String
+        
+        if isPossible(word: lowerCased){
+            if isOriginal(word: lowerCased){
+                if isReal(word: lowerCased){
+                    usedWords.insert(answer, at: 0)
+                    
+                    let indexPath = IndexPath(row: 0, section: 0)
+                    tableView.insertRows(at: [indexPath], with: .automatic)
+                    
+                    return
+                    
+                } else {
+                    errorTitle = "Word not recognized"
+                    errorMessage = "You can't just make them up, you know!"
+                }
+            } else {
+                errorTitle = "Word used already"
+                errorMessage = "Be more original!"
+            }
+        } else {
+            guard let title = title?.lowercased() else { return }
+            errorTitle = "Word not possible"
+            errorMessage = "You can't make up that word from \(title)"
+        }
+        
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
         
     }
     
@@ -52,7 +117,7 @@ class ViewController: UITableViewController {
         usedWords.removeAll(keepingCapacity: true)
         tableView.reloadData()
     }
-        
+    
     //MARK: - TableView Data Source Methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return usedWords.count
@@ -63,7 +128,6 @@ class ViewController: UITableViewController {
         cell.textLabel?.text = usedWords[indexPath.row]
         return cell
     }
-
-
+    
+    
 }
-
